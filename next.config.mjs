3d3242c8f05c28644cdeb554a4e35d67f6f30e38
tgzhafri <tgzhafri@gmail.com>
2023-11-01 15:50:21 +0800

    deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,11 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+  distDir: "build", // Ensure this matches your build output directory
 };
 
 export default withContentlayer(nextConfig);
